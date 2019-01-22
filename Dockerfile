1bb57ab33bd0eb4b8754afc53205cbdf70a7985d
Alvar Penning <post@0x21.biz>
2019-01-22 16:34:46 +0100

    Image generation was changed to modification

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,8 @@
 FROM debian:stretch-slim
 
-RUN apt-get update \
-  && apt-get install -y dosfstools git kpartx multistrap
+RUN apt-get update && \
+  apt-get install -y dosfstools git kpartx parted proot qemu qemu-user-static
 
-COPY multistrap.conf /
 COPY builder.sh /
 
 RUN chmod +x /builder.sh
--- builder.sh
+++ builder.sh
@@ -1,63 +1,66 @@
 #!/usr/bin/env bash
 set -e
 
-# create image and its partitions
-dd if=/dev/zero of=/result/rpi.img bs=1MB count=512
-LOOP=`losetup -P --show -f /result/rpi.img | sed -E 's/.*(loop[0-9])/\1/g'`
-fdisk "/dev/${LOOP}" << EOF
-o
-n
-p
-1
- 
-+64M
-t
-c
-n
-p
-2
- 
- 
-w
-EOF
-losetup -d "/dev/${LOOP}"
+# mount_image mounts the given image file as a loop device and "returns"/prints
+# the name of the loop device (e.g. loop0).
+# Usage: mount_image /tmp/raspi.img
+mount_image() {
+  kpartx -avs "$1" \
+    | sed -E 's/.*(loop[0-9])p.*/\1/g' \
+    | head -n 1
+}
 
-LOOP=`kpartx -avs /result/rpi.img | sed -E 's/.*(loop[0-9])p.*/\1/g' | head -n 1`
+# umount_image unmounts the given image file, mounted with mount_image.
+# Usage: umount_image /tmp/raspi.img
+umount_image() {
+  kpartx -d "$1"
+}
 
-LOOP_BOOT="/dev/mapper/${LOOP}p1"
-LOOP_ROOT="/dev/mapper/${LOOP}p2"
+# pump_image increases the size of the given image and fixes its partition
+# table.
+# Usage: pump_image PATH_TO_IMAGE PUMP_SIZE_IN_MB
+pump_image() {
+  dd if=/dev/zero of="$1" bs=1M count=$2 oflag=append conv=notrunc
 
-# create file systems
-mkfs.vfat $LOOP_BOOT
-mkfs.ext4 $LOOP_ROOT
+  local loop=`mount_image $1`
+  parted "/dev/${loop}" -- resizepart 2 -1s
+  umount_image $1
+}
+
+arm_chroot() {
+  proot -0 -q qemu-arm-static -w / -r /mnt/rootfs $@
+}
+
+
+pump_image "/result/rpi.img" 200
+
+
+#LOOP_BOOT="/dev/mapper/${LOOP}p1"
+#LOOP_ROOT="/dev/mapper/${LOOP}p2"
 
 # write files
-mkdir -p /mnt/bootfs
-mkdir -p /mnt/rootfs
+#mkdir -p /mnt/bootfs
+#mkdir -p /mnt/rootfs
 
-mount $LOOP_BOOT /mnt/bootfs
-mount $LOOP_ROOT /mnt/rootfs
+#mount $LOOP_BOOT /mnt/bootfs
+#mount $LOOP_ROOT /mnt/rootfs
 
-# multistrap straps a Debian/Raspbian to rootfs
-multistrap -d /mnt/rootfs -f /multistrap.conf
+# ricing
+# echo baumbox > /mnt/rootfs/etc/hostname
+# echo "127.0.0.1\tbaumbox" >> /mnt/rootfs/etc/hosts
 
-# install the RaspberryPi's firmware
-git clone --depth=1 https://github.com/raspberrypi/firmware.git /rpi-firmware
-cp -ar /rpi-firmware/hardfp/opt/* /mnt/rootfs/opt/
-cp -r /rpi-firmware/modules /mnt/rootfs/lib/
-cp -r /rpi-firmware/boot/* /mnt/bootfs/
+# cat > /mnt/rootfs/etc/fstab << EOF
+# /dev/mmcblk0p1 /boot vfat sync,dirsync         0 0
+# /dev/mmcblk0p2 /     ext4 sync,dirsync,noatime 0 0
+# proc           /proc proc defaults             0 0
+#
+# EOF
 
-cat > /mnt/bootfs/config.txt << EOF
-enable_uart=1
-EOF
+# arm_chroot /bin/systemctl disable getty@ttyS0.service
+# arm_chroot /bin/systemctl disable serial-getty@ttyS0.service
 
-cat > /mnt/bootfs/cmdline.txt << EOF
-dwc_otg.lpm_enable=0 console=serial0,115200 console=tty1 root=/dev/mmcblk0p2 rootfstype=ext4 elevator=deadline fsck.repair=yes rootwait init=/lib/systemd/systemd
-EOF
+#sync
 
-sync
+#umount /mnt/bootfs
+#umount /mnt/rootfs
 
-umount /mnt/bootfs
-umount /mnt/rootfs
-
-kpartx -d /result/rpi.img
