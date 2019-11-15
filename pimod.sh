502b719ed7586a9f02324c187aafc07100d21813
Jonas Höchst <git@jonashoechst.de>
2019-11-15 09:59:57 +0100

    parsing arguments using getopts

--- pimod.sh
+++ pimod.sh
@@ -7,9 +7,29 @@ for mod in ${PIMOD_BASE}/modules/*.sh; do
   . "${mod}"
 done
 
-if [[ -z ${1+x} ]]; then
-  echo "Usage: ${0} Pifile"
+show_help() {
+  cat <<EOF
+Usage: ${0} [Options] Pifile
+
+Options:
+  -h        Print this help message.
+EOF
+}
+
+while getopts "h" opt; do
+  case "$opt" in
+  h)
+    show_help
+    exit 0
+    ;;
+  esac
+done
+
+PIFILE=${@:$OPTIND:1}
+
+if [[ -z "${PIFILE}" ]]; then
+  show_help
   exit 1
 fi
 
-execute_pifile "${1}"
+execute_pifile "${PIFILE}"
